f50849203938dc91dca4a54ea297c0a2796d49cc
itsgabeover <gabe.enrique.miranda@gmail.com>
2021-06-18 22:48:26 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+wrapGifts(gifts); 
+*/
+
+function writeCards(people,event){
+
+    for (let i = 0; i < people.length; i++){
+        people[i] = `Thank you, ${people[i]}, for the wonderful ${event} gift!`;
+    }
+    return people;
+    
+}
+
+function countDown(count){
+    let i = count;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
